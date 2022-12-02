5f5e751bd89b4905ef4e601c7511f705a6c90369
soomin-world <ojudve123@gmail.com>
2022-12-02 10:45:54 +0900

    styled-component연습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
+// 1. styled-components를 만들었습니다.
+const STBox = styled.div`
+  width: 100px;
+  height: 100px;
+  border: 1px solid ${(props) => props.borderColor}; // 4.부모 컴포넌트에서 보낸 props를 받아 사용합니다.
+  margin: 20px;
+`;
+const boxList = ["red", "green", "blue"];
+
+// 색을 넣으면, 이름을 반환해주는 함수를 만듭니다.
+const getBoxName = (color) => {
+  switch (color) {
+    case "red":
+      return "빨간 박스";
+    case "green":
+      return "초록 박스";
+    case "blue":
+      return "파란 박스";
+    default:
+      return "검정 박스";
+  }
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {boxList.map((box) => (
+        <STBox borderColor={box}>{getBoxName(box)}</STBox>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
